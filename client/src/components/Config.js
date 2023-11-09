7d3847eed03014520017d9ce1df85856d3299f67
anechoicTree <anechoic77@gmail.com>
2023-11-09 19:39:48 +0900

    Part1. MovieDB API 데이터 STATE 연동 & MainImage Component 구현 / v 1.01

---
+++ client/src/components/Config.js
@@ -0,0 +1,7 @@
+//SERVER ROUTES
+export const USER_SERVER = '/api/users';
+
+//타 파일에서도 사용할 수 있게 export 선언을 해준다
+export const API_URL = "https://api.themoviedb.org/3/";
+export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
+export const API_KEY = "1a0b8b7d2765d9b7292310eefb6fb32c";
